6fa867a3409a3dbe53579d411549f1dfdee24dd6
Woron Mene <meneworon4@gmail.com>
2023-07-13 11:16:22 +0100

    change focus mode content

--- src/content.js
+++ src/content.js
@@ -2,6 +2,12 @@ const pageContent = document.getElementById("contents");
 const parentElement = pageContent.parentNode;
 const newElement = document.getElementById("newElement");
 
+const link = document.createElement("link");
+link.href = "https://fonts.googleapis.com/css2?family=Kodchasan&display=swap";
+link.rel = "stylesheet";
+
+document.head.appendChild(link);
+
 function removeHomepageContent() {
   console.log(window.location.href);
   if (window.location.href === "https://www.youtube.com/") {
@@ -9,23 +15,29 @@ function removeHomepageContent() {
     // Step 1: Create the new element
     const newElement = document.createElement("div");
     newElement.id = "newElement";
-    const giphyContainer = document.createElement("div");
-
-    giphyContainer.innerHTML = `
-<iframe src="https://giphy.com/embed/mC7g4euY2YcTjHbTfL" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-
-  
-`;
     newElement.style.height = "80vh";
     newElement.style.width = "80vh";
     newElement.style.display = "flex";
     newElement.style.justifyContent = "center";
 
     newElement.style.alignItems = "center";
-    newElement.style.fontSize = "24px";
-    // newElementText.style.color = "white";
-    // newElement.appendChild(newElementText);
-    newElement.appendChild(giphyContainer);
+    const newElementText = document.createElement("p");
+    newElementText.className = "newElementText";
+    newElementText.style.fontSize = "30px";
+    newElementText.style.fontFamily = "Kodchasan";
+    newElementText.style.color = "white";
+    newElementText.innerText = "Time to focus";
+    newElement.appendChild(newElementText);
+    //     const giphyContainer = document.createElement("div");
+
+    //     giphyContainer.innerHTML = `
+    // <iframe src="https://giphy.com/embed/mC7g4euY2YcTjHbTfL" width="300" height="300" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+    // `;
+
+    //     newElement.style.fontSize = "24px";
+
+    //     newElement.appendChild(giphyContainer);
 
     // Step 2: Get a reference to the existing element
 
